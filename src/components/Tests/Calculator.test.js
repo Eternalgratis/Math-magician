6125d2580be43003ced488f3bbe1ccb9427c6293
Aneal Laryea <aneallaryea100@gmail.com>
2022-08-17 21:28:00 +0000

    fix calculate test

--- src/components/Tests/Calculator.test.js
+++ src/components/Tests/Calculator.test.js
@@ -1,7 +1,28 @@
-import Renderer from 'react-test-renderer';
-import Calculator from '../Calculator';
+import calculate from '../../logic/calculate';
 
-it('Renders Calculator component', () => {
-  const tree = Renderer.create(<Calculator />).toJSON();
-  expect(tree).toMatchSnapshot();
+describe('Renders Calculator component', () => {
+  test('test calculate objects and test contents', () => {
+    const obj1 = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+
+    const obj2 = {
+      total: '0',
+      next: '0',
+      operation: '+',
+    };
+
+    const buttonName1 = 'AC';
+    const buttonName2 = '0';
+
+    expect(calculate(obj1, buttonName1)).toMatchObject({
+      total: null,
+      next: null,
+      operation: null,
+    });
+
+    expect(calculate(obj2, buttonName2)).toMatchObject({});
+  });
 });
