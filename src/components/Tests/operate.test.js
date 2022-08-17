21a83cedfb7e5d856e3a68e17ffc5dfd7e18d859
Aneal Laryea <aneallaryea100@gmail.com>
2022-08-17 20:46:47 +0000

    testing operator

---
+++ src/components/Tests/operate.test.js
@@ -0,0 +1,24 @@
+import Renderer from 'react-test-renderer';
+import operate from '../../logic/operate';
+
+describe('operation test', () => {
+    test('test the add operator', () => {
+        const addOperator = operate(5,5,'+');
+        expect(addOperator).toBe('10');
+    })
+
+    test('test for substraction', () => {
+        const substractOperator = operate(15,5,'-');
+        expect(substractOperator).toBe('10');
+    })
+
+    test('test for multiplication', () => {
+        const multiplyOperator = operate(15,2,'x');
+        expect(multiplyOperator).toBe('30');
+    })
+
+    test('test for divide', () => {
+        const divideOperator = operate(15,5,'÷');
+        expect(divideOperator).toBe('3');
+    })
+})
